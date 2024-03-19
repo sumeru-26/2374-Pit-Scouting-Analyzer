aa590bc83a04c6aa5f80acb406a6bf1f1b03fdba
Sumeru Gowda <155037091+sumeru-26@users.noreply.github.com>
2024-03-19 00:22:12 +0000

    updated decompression

--- file.py
+++ file.py
@@ -1,12 +1,12 @@
 import csv
 import pandas as pd
 
-# fields = ['Team Number', 'Drive Train', 'Intake Type', 'Indexing Type', '# of Notes in Auto', 'Notes about Auto', 'Endgame', 'Alliance Criteria']
+fields = ['Drive Train', 'Intake', 'Shooter', 'Climber', 'Team Number', 'Auto Notes #', 'Notes About Auto', 'What They Want On Alliance', 'Other Notes'] 
 
 def add(coded_str : str, file_name : str):
     with open(file_name, 'a') as file:
         writer = csv.writer(file)
-        row = coded_str.split('^')
+        row = decode_str(coded_str)
         writer.writerow(row)
 
 def read(file_name : str):
@@ -14,3 +14,20 @@ def read(file_name : str):
 
 def update(df : pd.DataFrame, file_name : str):
     df.to_csv(file_name)
+
+def decode_str(str : str) -> list[str]:
+    str = str.lower()
+    re = str.split('$')
+    radios = list(re[0])
+    del re[0]
+    return radios + re
+
+# NOTE: WILL DELETE ALL SAVED DATA
+def rewrite_header(fields : list[str], file_name : str):
+    with open(file_name, 'w') as file:
+        pass
+    with open(file_name, 'a') as file:
+        csv.writer(file).writerow(fields)
+
+test = '1133$2374$4$VERY CONSISTENT ON BOTH SIDES$A FAST SPEAKER BOT$SINGLE ARM MECHANISM, FAST AMP CYCLES, AUTO SHOOTING'
+add(test,'backup.csv')    
